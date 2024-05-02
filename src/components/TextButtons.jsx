475089dbd8acf21a9b6ab7ecd4f2aebe0fc0beaa
Hartz-byte <harshmail281199@gmail.com>
2024-05-02 17:47:56 +0530

    update

---
+++ src/components/TextButtons.jsx
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { useMediaQuery } from "@mui/material/";
+
+const TextButtons = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleClick = (index) => {
+    setActiveButton(index);
+  };
+
+  const isMobile = useMediaQuery("(max-width: 1233px)");
+
+  useEffect(() => {
+    setActiveButton(2);
+  }, []);
+
+  return (
+    <Box
+      display={isMobile ? "" : "flex"}
+      justifyContent="space-between"
+      alignItems="center"
+      mt={2}
+      mb={2}
+      gap={5}
+    >
+      {/* Text */}
+      <Box ml={30} mr={isMobile ? 30 : ""} mb={isMobile ? 2 : 0}>
+        <Typography textAlign="center">
+          Dailymed Drug Database: The DailyMed database contains 143072
+          labelling submitted to the Food and Drug Administration (DFA) by
+          companies. DailyMed does not contain a complete listing of labelling
+          for DFA-regulated products (e.g. labelling that is not submitted to
+          the FDA).
+        </Typography>
+      </Box>
+
+      {/* Buttons */}
+      <Box
+        display="flex"
+        flexDirection={isMobile ? "column" : "column"}
+        alignItems={isMobile ? "center" : ""}
+        gap={1}
+        mr={20}
+      >
+        <Box display="flex" gap={1}>
+          <Button
+            variant="outlined"
+            sx={{
+              width: "150px",
+              color: "black",
+              borderColor: "black",
+              textTransform: "none",
+              backgroundColor: activeButton === 0 ? "#DBF2FF" : "white",
+            }}
+            onClick={() => handleClick(0)}
+          >
+            Anesthesisa
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              width: "150px",
+              color: "black",
+              borderColor: "black",
+              textTransform: "none",
+              backgroundColor: activeButton === 1 ? "#DBF2FF" : "white",
+            }}
+            onClick={() => handleClick(1)}
+          >
+            Cardiology
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              width: "150px",
+              color: "black",
+              borderColor: "black",
+              textTransform: "none",
+              backgroundColor: activeButton === 2 ? "#DBF2FF" : "white",
+            }}
+            onClick={() => handleClick(2)}
+          >
+            Critical Care
+          </Button>
+        </Box>
+
+        <Box display="flex" gap={1}>
+          <Button
+            variant="outlined"
+            sx={{
+              width: "150px",
+              color: "black",
+              borderColor: "black",
+              textTransform: "none",
+              backgroundColor: activeButton === 3 ? "#DBF2FF" : "white",
+            }}
+            onClick={() => handleClick(3)}
+          >
+            Anesthesisa
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              width: "150px",
+              color: "black",
+              borderColor: "black",
+              textTransform: "none",
+              backgroundColor: activeButton === 4 ? "#DBF2FF" : "white",
+            }}
+            onClick={() => handleClick(4)}
+          >
+            Cardiology
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              width: "150px",
+              color: "black",
+              borderColor: "black",
+              textTransform: "none",
+              backgroundColor: activeButton === 5 ? "#DBF2FF" : "white",
+            }}
+            onClick={() => handleClick(5)}
+          >
+            Critical Care
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default TextButtons;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box } from "@mui/material";
 import Header from "../components/Header";
 import TopBox from "../components/TopBox";
+import TextButtons from "../components/TextButtons";
 
 const MainPage = () => {
   return (
@@ -11,6 +12,9 @@ const MainPage = () => {
 
       {/* top colored box */}
       <TopBox />
+
+      {/* text and buttons */}
+      <TextButtons />
     </Box>
   );
 };
