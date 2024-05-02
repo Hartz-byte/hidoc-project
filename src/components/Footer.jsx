c4a989147ab186e10ba59e46eb92b82dbe903577
Hartz-byte <harshmail281199@gmail.com>
2024-05-03 02:40:32 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,6 +12,40 @@ import CalculateIcon from "@mui/icons-material/Calculate";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 
+// features box component
+const FeatureBox = ({ icon, color, backColor, title }) => (
+  // main container
+  <Box
+    width={"140px"}
+    height={"140px"}
+    backgroundColor={color}
+    display={"flex"}
+    flexDirection={"column"}
+    justifyContent={"center"}
+    alignItems={"center"}
+    gap={1}
+    sx={{ cursor: "pointer" }}
+  >
+    {/* icon inclosed in a circle */}
+    <Box
+      width={"40px"}
+      height={"40px"}
+      borderRadius={"50%"}
+      backgroundColor={backColor}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      {icon}
+    </Box>
+
+    {/* title */}
+    <Typography color={backColor} fontSize={15}>
+      {title}
+    </Typography>
+  </Box>
+);
+
 const Footer = () => {
   return (
     <Box
@@ -19,74 +53,68 @@ const Footer = () => {
       height={"400px"}
       backgroundColor={"#091734"}
       display={"flex"}
+      gap={8}
     >
-      {/* left and mid sections */}
-      <Box maxWidth={"50%"} display={"flex"} ml={5} gap={10}>
-        {/* left section */}
-        <Box
-          display={"flex"}
-          flexDirection={"column"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          gap={2}
-        >
-          {/* logo */}
-          <img
-            src={Logo}
-            alt="Logo"
-            style={{ width: "270px", height: "120px" }}
-          />
-
-          {/* text */}
-          <Typography color={"white"} width={"270px"}>
-            Fastest Growing Medical Platform for Doctors.
-          </Typography>
-
-          {/* icons */}
-          <Box display={"flex"} gap={2}>
-            <FacebookIcon sx={{ color: "white" }} />
-            <InstagramIcon sx={{ color: "white" }} />
-            <YouTubeIcon sx={{ color: "white" }} />
-            <LinkedInIcon sx={{ color: "white" }} />
-          </Box>
-        </Box>
-
-        {/* mid section */}
-        <Box
-          display={"flex"}
-          flexDirection={"column"}
-          justifyContent={"center"}
-          gap={2}
-        >
-          <Typography fontWeight={"bold"} color={"white"} fontSize={18}>
-            REACH US
-          </Typography>
-
-          <Typography color={"white"}>
-            Please contact the below details for any other information.
-          </Typography>
-
-          <Typography color={"#0582B0"}>Email:</Typography>
-
-          <Typography color={"white"}>info@hidoc.co</Typography>
-
-          <Typography color={"#0582B0"}>Address:</Typography>
-
-          <Typography color={"white"}>
-            Hidoc Dr. Inc, 2055 Limestone Rd, STE 200-C, Wilmington, DE, 19808,
-            United States.
-          </Typography>
-        </Box>
-      </Box>
-
-      {/* right section */}
+      {/* left section */}
       <Box
-        width={"50%"}
-        ml={5}
         display={"flex"}
         flexDirection={"column"}
         justifyContent={"center"}
+        alignItems={"center"}
+        gap={2}
+        ml={5}
       >
+        {/* logo */}
+        <img
+          src={Logo}
+          alt="Logo"
+          style={{ width: "270px", height: "120px" }}
+        />
+
+        {/* text */}
+        <Typography color={"white"} width={"270px"}>
+          Fastest Growing Medical Platform for Doctors.
+        </Typography>
+
+        {/* icons */}
+        <Box display={"flex"} gap={2}>
+          <FacebookIcon sx={{ color: "white" }} />
+          <InstagramIcon sx={{ color: "white" }} />
+          <YouTubeIcon sx={{ color: "white" }} />
+          <LinkedInIcon sx={{ color: "white" }} />
+        </Box>
+      </Box>
+
+      {/* mid section */}
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        gap={2}
+        width={"450px"}
+      >
+        <Typography fontWeight={"bold"} color={"white"} fontSize={18}>
+          REACH US
+        </Typography>
+
+        <Typography color={"white"}>
+          Please contact the below details for any other information.
+        </Typography>
+
+        <Typography color={"#0582B0"}>Email:</Typography>
+
+        <Typography color={"white"}>info@hidoc.co</Typography>
+
+        <Typography color={"#0582B0"}>Address:</Typography>
+
+        <Typography color={"white"}>
+          Hidoc Dr. Inc, 2055 Limestone Rd, STE 200-C, Wilmington, DE, 19808,
+          United States.
+        </Typography>
+      </Box>
+
+      {/* right section */}
+      <Box display={"flex"} flexDirection={"column"} justifyContent={"center"}>
         {/* text */}
         <Typography fontWeight={"bold"} color={"white"} mb={2}>
           HIDOC DR. FEATURES
@@ -97,239 +125,71 @@ const Footer = () => {
           {/* upper row */}
           <Box display={"flex"}>
             {/* drug database */}
-            <Box
-              width={"140px"}
-              height={"140px"}
-              backgroundColor={"#D9F3FF"}
-              display={"flex"}
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              gap={1}
-              sx={{cursor: "pointer"}}
-            >
-              <Box
-                width={"40px"}
-                height={"40px"}
-                borderRadius={"50%"}
-                backgroundColor={"#40C4FF"}
-                display={"flex"}
-                justifyContent={"center"}
-                alignItems={"center"}
-              >
-                <FolderOpenIcon sx={{ color: "white" }} />
-              </Box>
-
-              <Typography color={"#40C4FF"} fontSize={15}>
-                Drug Database
-              </Typography>
-            </Box>
+            <FeatureBox
+              icon={<FolderOpenIcon sx={{ color: "white" }} />}
+              color="#D9F3FF"
+              title="Drug Database"
+              backColor="#40C4FF"
+            />
 
             {/* Webinars */}
-            <Box
-              width={"140px"}
-              height={"140px"}
-              backgroundColor={"#FDD9D6"}
-              display={"flex"}
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              gap={1}
-              sx={{cursor: "pointer"}}
-            >
-              <Box
-                width={"40px"}
-                height={"40px"}
-                borderRadius={"50%"}
-                backgroundColor={"#F44336"}
-                display={"flex"}
-                justifyContent={"center"}
-                alignItems={"center"}
-              >
-                <VideoCallIcon sx={{ color: "white" }} />
-              </Box>
-
-              <Typography color={"#F44336"} fontSize={15}>
-                Webinars
-              </Typography>
-            </Box>
+            <FeatureBox
+              icon={<VideoCallIcon sx={{ color: "white" }} />}
+              color="#FDD9D6"
+              title="Webinars"
+              backColor="#F44336"
+            />
 
             {/* drug interaction */}
-            <Box
-              width={"140px"}
-              height={"140px"}
-              backgroundColor={"#D9E8FF"}
-              display={"flex"}
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              gap={1}
-              sx={{cursor: "pointer"}}
-            >
-              <Box
-                width={"40px"}
-                height={"40px"}
-                borderRadius={"50%"}
-                backgroundColor={"#448AFF"}
-                display={"flex"}
-                justifyContent={"center"}
-                alignItems={"center"}
-              >
-                <DescriptionIcon sx={{ color: "white" }} />
-              </Box>
-
-              <Typography color={"#448AFF"} fontSize={15}>
-                Drug Interaction
-              </Typography>
-            </Box>
+            <FeatureBox
+              icon={<DescriptionIcon sx={{ color: "white" }} />}
+              color="#D9E8FF"
+              title="Drug Interaction"
+              backColor="#448AFF"
+            />
 
             {/* medical calculator */}
-            <Box
-              width={"140px"}
-              height={"140px"}
-              backgroundColor={"#DBEFDC"}
-              display={"flex"}
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              gap={1}
-              sx={{cursor: "pointer"}}
-            >
-              <Box
-                width={"40px"}
-                height={"40px"}
-                borderRadius={"50%"}
-                backgroundColor={"#4CAF50"}
-                display={"flex"}
-                justifyContent={"center"}
-                alignItems={"center"}
-              >
-                <CalculateIcon sx={{ color: "white" }} />
-              </Box>
-
-              <Typography color={"#4CAF50"} fontSize={15}>
-                Medical Calculator
-              </Typography>
-            </Box>
+            <FeatureBox
+              icon={<CalculateIcon sx={{ color: "white" }} />}
+              color="#DBEFDC"
+              title="Medical Calculator"
+              backColor="#4CAF50"
+            />
           </Box>
 
           {/* down row */}
           <Box display={"flex"}>
             {/* Articles */}
-            <Box
-              width={"140px"}
-              height={"140px"}
-              backgroundColor={"#FFEACC"}
-              display={"flex"}
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              gap={1}
-              sx={{cursor: "pointer"}}
-            >
-              <Box
-                width={"40px"}
-                height={"40px"}
-                borderRadius={"50%"}
-                backgroundColor={"#40C4FF"}
-                display={"flex"}
-                justifyContent={"center"}
-                alignItems={"center"}
-              >
-                <DescriptionIcon sx={{ color: "white" }} />
-              </Box>
-
-              <Typography color={"#40C4FF"} fontSize={15}>
-                Articles
-              </Typography>
-            </Box>
+            <FeatureBox
+              icon={<DescriptionIcon sx={{ color: "white" }} />}
+              color="#FFEACC"
+              title="Articles"
+              backColor="#FF9800"
+            />
 
             {/* Quizzes */}
-            <Box
-              width={"140px"}
-              height={"140px"}
-              backgroundColor={"#D9E8FF"}
-              display={"flex"}
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              gap={1}
-              sx={{cursor: "pointer"}}
-            >
-              <Box
-                width={"40px"}
-                height={"40px"}
-                borderRadius={"50%"}
-                backgroundColor={"#40C4FF"}
-                display={"flex"}
-                justifyContent={"center"}
-                alignItems={"center"}
-              >
-                <EmojiEventsIcon sx={{ color: "white" }} />
-              </Box>
-
-              <Typography color={"#40C4FF"} fontSize={15}>
-                Quizzes
-              </Typography>
-            </Box>
+            <FeatureBox
+              icon={<EmojiEventsIcon sx={{ color: "white" }} />}
+              color="#D9E8FF"
+              title="Quizzes"
+              backColor="#448AFF"
+            />
 
             {/* Diseases Database */}
-            <Box
-              width={"140px"}
-              height={"140px"}
-              backgroundColor={"#D9F3FF"}
-              display={"flex"}
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              gap={1}
-              sx={{cursor: "pointer"}}
-            >
-              <Box
-                width={"40px"}
-                height={"40px"}
-                borderRadius={"50%"}
-                backgroundColor={"#40C4FF"}
-                display={"flex"}
-                justifyContent={"center"}
-                alignItems={"center"}
-              >
-                <DescriptionIcon sx={{ color: "white" }} />
-              </Box>
-
-              <Typography color={"#40C4FF"} fontSize={15}>
-                Diseases Database
-              </Typography>
-            </Box>
+            <FeatureBox
+              icon={<DescriptionIcon sx={{ color: "white" }} />}
+              color="#D9F3FF"
+              title="Diseases Database"
+              backColor="#40C4FF"
+            />
 
             {/* surveys */}
-            <Box
-              width={"140px"}
-              height={"140px"}
-              backgroundColor={"#FDD9D6"}
-              display={"flex"}
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              gap={1}
-              sx={{cursor: "pointer"}}
-            >
-              <Box
-                width={"40px"}
-                height={"40px"}
-                borderRadius={"50%"}
-                backgroundColor={"#F44336"}
-                display={"flex"}
-                justifyContent={"center"}
-                alignItems={"center"}
-              >
-                <AssignmentIcon sx={{ color: "white" }} />
-              </Box>
-
-              <Typography color={"#F44336"} fontSize={15}>
-                Surveys
-              </Typography>
-            </Box>
+            <FeatureBox
+              icon={<AssignmentIcon sx={{ color: "white" }} />}
+              color="#FDD9D6"
+              title="Surveys"
+              backColor="#F44336"
+            />
           </Box>
         </Box>
       </Box>
